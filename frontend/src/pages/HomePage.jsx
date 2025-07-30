ea777256a1d9c51cf0304b7a1ff647068eb0f69f
Mark Adrian Reyes <markadrianreyes65@gmail.com>
2025-07-30 19:17:46 +0800

    Ready for deployment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -45,11 +45,11 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         { loading && <div className="text-center text-[#3D43F3] py-10">Loading...</div> }
 
-        {notes.length === 0 && !isRateLimited && !loading && <NotesNotFound />}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"> 
-            {notes.map(note => (
+            {notes.map((note) => (
               <NoteCard key={note._id} note={note} setNotes={setNotes} /> 
             ))}
           </div>
