1b566e0f17ac03b320cce37eab52c16ead679317
Mark Adrian Reyes <markadrianreyes65@gmail.com>
2025-07-31 11:02:44 +0800

    updated code

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -40,12 +40,12 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      { isRateLimited && <RateLimitedUI /> }
+      {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        { loading && <div className="text-center text-[#3D43F3] py-10">Loading...</div> }
+        {loading && <div className="text-center text-[#3D43F3] py-10">Loading...</div>}
 
-        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+        {!loading && notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"> 
